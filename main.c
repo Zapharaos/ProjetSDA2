012906011aa6ac04b2d2aad654192d2e5fe89a99
Zapharaos <mfreitag@numericable.fr>
2020-10-20 15:37:54 +0200

    update main

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
+#include "utils.c"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> // strtok()
 
 #define nb_word_max 64
-#define sentence_max_size 256
+#define SENTENCE_MAX_SIZE 256
 
  /**
   * \fn      void err_print()
@@ -25,22 +27,30 @@ void pas_content(char *message) {
     exit(1);
 }
 
+/**
+ * \fn      char** get_sentence(size_t* n)
+ * \brief   Retourne un tableau 2D de la phrase
+ * \param   *n Le nombre de mots dans la phrase
+ */
 char** get_sentence(size_t* n) {
     
-    const char * separators = " ,.?!\n";
-    char s[sentence_max_size] = "";
+    char s[SENTENCE_MAX_SIZE] = "";
+    const char * separators = " ,.?!\n"; // split at this chars
     
-    fprintf(stdout, "Votre phrase : ");
-    if(fgets(s, sentence_max_size, stdin) == NULL) {
+    if(fprintf(stdout, "Votre phrase : ") < 0)
+        err_print();
+    
+    if(fgets(s, SENTENCE_MAX_SIZE, stdin) == NULL)
         pas_content("fgets sentence");
-    }
     
-    char** sentence = malloc(sizeof(char) * 64);
-    char * token = strtok (s, separators);
+    char** sentence = (char**) malloc(sizeof(char*) * (*n)); // sizeof(char) = 1
+    char* token = strtok(s, separators);
+    
     do {
+        to_lower(token);
         sentence[(*n)++] = token;
-    } while ( (token = strtok (NULL, separators)) != NULL );
-    
+    } while ((token = strtok(NULL, separators)) != NULL );
+
     return sentence;
 }
 
@@ -48,13 +58,12 @@ int main(int argc, char* argv[]) {
     
     if(argc != 1) {}
     
-    size_t n = 0;
-    char** sentence = get_sentence(&n);
+    size_t n = 0; // number of words
+    char** sentence = get_sentence(&n); // 2D tab with the words
     
     for(size_t i=0; i<n; i++)
-        printf("%s\n",sentence[i]);
-
-
+        if(fprintf(stdout, "%s\n", sentence[i]) < 0)
+            err_print();
     
     return 0;
 }
