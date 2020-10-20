69621e52db46ff2ad16e5fb63a9c8f33c6edb167
SchawnnDev <iperskill@gmail.com>
2020-10-20 11:13:01 +0200

    to lower

--- utils.c
+++ utils.c
@@ -6,9 +6,9 @@ void to_lower(char* entry)
 
     for (size_t i = 0; entry[i] != '\0'; i++)
     {
-        if (entry[i] < 'a' || entry[i] > 'z')
+        if (entry[i] > 'a' || entry[i] < 'z')
 	        continue;
     	
-        entry[i] = (char) (entry[i] - ('a' - 'A'));
+        entry[i] = (char) (entry[i] - ('A' - 'a'));
     }
 }
