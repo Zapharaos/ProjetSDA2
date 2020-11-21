3363105f75dc2bf7faeeb906a92f3769633e1b2c
Zapharaos <mfreitag@numericable.fr>
2020-11-21 15:51:32 +0100

    edit dawg struct + prefix length function

--- src/data/dawg.c
+++ src/data/dawg.c
@@ -5,13 +5,13 @@ Dawg empty_dawg()
 {
 	Dawg dawg = malloc(sizeof(struct dawg));
 
-	dawg->id = 0; // unknown
-	dawg->is_word = false;
-	dawg->neighbors = malloc(sizeof(struct vertex) * ALPHABET_SIZE);
+	dawg->root->id = 0; // unknown
+	dawg->root->is_word = false;
+	dawg->root->neighbors = malloc(sizeof(struct vertex) * ALPHABET_SIZE);
 	
 	// on initialise le tableau avec des pointeurs nuls
 	for (size_t i = 0; i < ALPHABET_SIZE; ++i)
-		dawg->neighbors[i] = NULL;
+		dawg->root->neighbors[i] = NULL;
 	
 	return dawg;
 }
@@ -23,15 +23,30 @@ void free_dawg(Dawg dawg)
 
 	for (size_t i = 0; i < ALPHABET_SIZE; ++i)
 	{
-		if(dawg->neighbors[i] == NULL)
+		if(dawg->root->neighbors[i] == NULL)
 			continue;
 
-		free_dawg(dawg->neighbors[i]->from);
-		free_dawg(dawg->neighbors[i]->to);
+		free_dawg(dawg->root->neighbors[i]->from);
+		free_dawg(dawg->root->neighbors[i]->to);
 	}
 		
-	free(dawg->neighbors);
+	free(dawg->root->neighbors);
 	free(dawg);
+}
+
+size_t search_prefix_length(char* word1, char* word2){
+
+    if(word1 == NULL || word2 == NULL)
+        return 0;
+
+    size_t index = 0;
+    size_t max = strlen(word1) > strlen(word2) ? strlen(word2) : strlen(word1);
+
+    while((index < max) && (word1[index] == word2[index])){
+    	index++;
+	}
+
+	return index;
 }
 
 /* TANT QUE la taille de la pile est supérieure à p
@@ -42,14 +57,14 @@ void free_dawg(Dawg dawg)
     	SINON
 			Enregistrer ce sommet dans la hashmap. FIN SI
 	FIN TANT QUE */
-void minimiser(Dawg dawg, struct stack *stack, struct hashmap_s *hashmap, size_t p){
-	while(stack_size(stack) > p){
-		void *a = stack_peek(stack); // changer le type de a ?
-		stack_pop(stack); 
-		if(HASHMAP_NULL == hashmap_get(hashmap, "jesaispasquoimettre", p)) // changer key et p ?
-			hashmap_remove(hashmap, "toujourspas", p); // changer key et p ?
+void minimiser(Dawg dawg, size_t p){
+	while(stack_size(dawg->stack) > p){
+		void *a = stack_peek(dawg->stack); // changer le type de a ?
+		stack_pop(dawg->stack); 
+		if(HASHMAP_NULL == hashmap_get(dawg->hashmap, "jesaispasquoimettre", p)) // changer key et p ?
+			hashmap_remove(dawg->hashmap, "toujourspas", p); // changer key et p ?
 		else
-			hashmap_put(hashmap, "encore?mdr", p, a); // changer key et p ?
+			hashmap_put(dawg->hashmap, "encore?mdr", p, a); // changer key et p ?
 	}
 }
 
